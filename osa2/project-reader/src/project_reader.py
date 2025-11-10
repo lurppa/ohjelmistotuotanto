63dcfc51df1066c52b131217d213ff9c889f06c0
Lari Nurkka <lari.nurkka@gmail.com>
2025-11-10 23:15:33 +0200

    hiottu project reader

--- osa2/project-reader/src/project_reader.py
+++ osa2/project-reader/src/project_reader.py
@@ -13,7 +13,9 @@ class ProjectReader:
         data = toml.loads(content)
         name = data["tool"]["poetry"]["name"]
         description = data["tool"]["poetry"]["description"]
+        license = data["tool"]["poetry"]["license"]
+        authors = data["tool"]["poetry"]["authors"]
         dependencies = data["tool"]["poetry"]["dependencies"].keys()
         dev_dependencies = data["tool"]["poetry"]["group"]["dev"]["dependencies"].keys()
 
-        return Project(name, description, dependencies, dev_dependencies)
+        return Project(name, description, license, authors, dependencies, dev_dependencies)
